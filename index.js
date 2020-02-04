aa2b0b466401e499259e6f76e7f8b65e86639764
John Travers <johnk.travers@gmail.com>
2020-02-04 13:44:37 -0700

    Configure app to use GraphQL schema and GraphiQL IDE

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 // Allows express to understand graphql and interact with graphql API
 // It is used as middleware for a single route
-const graphqlHTTP = require('express-graphql')
+const graphqlHTTP = require('express-graphql');
+const schema = require('./lib/schema/schema')
 
 
 app.use('/graphql-pets', graphqlHTTP({
-
+  schema: schema,
+  graphiql: true
 }))
 
 
-app.listen(3001, ()=> {
+app.listen(3001, () => {
   console.log("Listening for requests on port 3001")
 });
